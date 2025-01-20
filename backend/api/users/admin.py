c7fe2baa0d59874bdea391c55251cfcf7deec3f3
Abdulrhman Sayed Ali <abdulrhmansayedali@gmail.com>
2025-01-20 17:53:54 +0300

    add user type to admin

--- backend/api/users/admin.py
+++ backend/api/users/admin.py
@@ -30,6 +30,7 @@ class UserAdmin(BaseUserAdmin, AuditModelAdmin):
                     "whatsapp",
                     "image",
                     "about",
+                    "type",
                 )
             },
         ),
