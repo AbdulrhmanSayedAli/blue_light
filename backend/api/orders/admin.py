e4d6ce7f4a4ee2bf27b4687d04025ab66decb86b
Abdulrhman Sayed Ali <abdulrhmansayedali@gmail.com>
2025-02-01 15:08:09 +0300

    finish order from admin

--- backend/api/orders/admin.py
+++ backend/api/orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from common.audit.admin import AuditModelAdmin
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+from .models import Order, OrderCourse
 
-# Register your models here.
+
+class OrderCourseInline(NestedStackedInline):
+    model = OrderCourse
+    extra = 1
+    fields = [
+        "course",
+        "include_videos",
+        "include_files",
+        "include_quizzes",
+        "price",
+        "expires_at",
+    ]
+
+
+@admin.register(Order)
+class OrderAdmin(NestedModelAdmin, AuditModelAdmin):
+    inlines = [OrderCourseInline]
--- backend/api/orders/models.py
+++ backend/api/orders/models.py
@@ -3,6 +3,8 @@ from common.audit.models import HistoricalAuditModel
 from users.models import User
 from enums.enums import PaymentStatus
 from courses.models import Course
+from django.core.validators import MinValueValidator
+from django.utils.timezone import now
 
 
 class Order(HistoricalAuditModel):
@@ -14,3 +16,14 @@ class Order(HistoricalAuditModel):
 
 class OrderCourse(HistoricalAuditModel):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="courses")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="order_courses")
+    include_videos = models.BooleanField(default=False)
+    include_files = models.BooleanField(default=False)
+    include_quizzes = models.BooleanField(default=False)
+    price = models.DecimalField(decimal_places=2, max_digits=8, validators=[MinValueValidator(0.01)])
+    expires_at = models.DateTimeField()
+
+    @property
+    def is_expired(self):
+        """Returns True if the expiration time has passed, False otherwise."""
+        return self.expires_at < now()
